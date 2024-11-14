538517110159d43e4ab9cedb2e944fa93cdea79f
astro <astronomy.dev@gmail.com>
2024-11-14 19:54:01 +0200

    added tests for the paddle

---
+++ src/tests/paddle_test.py
@@ -0,0 +1,23 @@
+import unittest
+from sprites.paddle import Paddle
+from config import window_size, game_window, speed
+
+class TestPaddle(unittest.TestCase):
+    def setUp(self):
+        self.start_x = window_size[0]-100
+        self.start_y = window_size[1]//2
+        self.paddle = Paddle(window_size, game_window, self.start_x, self.start_y, speed=speed, color=(0, 255, 0))
+
+    def test_paddle_in_correct_x_position_at_start(self):
+        self.assertEqual(self.start_x, self.paddle.x)
+    
+    def test_paddle_in_correct_y_position_at_start(self):
+        self.assertEqual(self.start_y, self.paddle.y)
+
+    def test_paddle_moves_correct_amount_on_up_input(self):
+        self.paddle.update(1)
+        self.assertEqual(self.start_y - 5, self.paddle.y)
+
+    def test_paddle_moves_correct_amount_on_down_input(self):
+        self.paddle.update(-1)
+        self.assertEqual(self.start_y + 5, self.paddle.y)
