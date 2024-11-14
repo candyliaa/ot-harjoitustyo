a89d8ffc4ac98463d8a2edaaccab34ff5f8342f8
astro <astronomy.dev@gmail.com>
2024-11-14 22:11:01 +0200

    tasks now checks used os

--- tasks.py
+++ tasks.py
@@ -1,14 +1,17 @@
+import os
 from invoke import task
+
+use_pty = os.name != "nt"
 
 @task
 def start(ctx):
-    ctx.run("python3 src/app.py", pty=True)
+    ctx.run("python src/app.py", pty=use_pty)
 
 @task
 def test(ctx):
-    ctx.run("pytest src", pty=True)
+    ctx.run("pytest src", pty=use_pty)
 
 @task
 def coverage_report(ctx):
-    ctx.run("coverage run --branch -m pytest src", pty=True)
-    ctx.run("coverage html", pty=True)
+    ctx.run("coverage run --branch -m pytest src", pty=use_pty)
+    ctx.run("coverage html", pty=use_pty)
