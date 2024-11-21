48ccec8c313618c325467d97c5d5ecc212cf0eaf
astro <astronomy.dev@gmail.com>
2024-11-21 19:12:59 +0200

    position and direction are now vectors for better collition detection

--- src/sprites/ball.py
+++ src/sprites/ball.py
@@ -1,15 +1,12 @@
 import pygame
 
-
 class Ball:
     def __init__(self, x, y, speed=10, color=(255, 255, 255), size=10):
         self.__initial_x = x
         self.__initial_y = y
-        self.x = x
-        self.y = y
 
-        self.x_dir = 1
-        self.y_dir = 1
+        self.position = pygame.math.Vector2(x, y)
+        self.direction = pygame.math.Vector2(1, 1)
 
         self.speed = speed
         self.color = color
@@ -17,36 +14,36 @@ class Ball:
         self.size = size
 
     def update(self, window_size):
-        if self.x <= 0 - self.size:
+        if self.position.x <= 0 - self.size:
             self.reset()
             scored = "own"
-        elif self.x >= window_size[0] + self.size:
+        elif self.position.x >= window_size[0] + self.size:
             self.reset()
             scored = "enemy"
         else:
             scored = None
 
-        if self.y <= 0 + self.size:
-            self.y_dir = 1
-        elif self.y >= window_size[1] - self.size:
-            self.y_dir = -1
+        if self.position.y <= 0 + self.size or self.position.y >= window_size[1] - self.size:
+            self.direction.y = -self.direction.y
 
-        self.x += self.speed * self.x_dir
-        self.y += self.speed * self.y_dir
+        self.position += self.speed * self.direction
 
         return scored
 
     def draw_ball(self, game_window):
         pygame.draw.circle(game_window, self.color,
-                           (self.x, self.y), self.size)
+                           (self.position.x, self.position.y), self.size)
 
     def get_ball_rect(self):
-        return pygame.Rect(self.x - self.size, self.y - self.size, self.size * 2, self.size * 2)
+        return pygame.Rect(self.position.x - self.size, self.position.y - self.size, self.size * 2, self.size * 2)
 
-    def collision(self):
-        self.x_dir *= -1
+    def collision(self, paddle):
+        if abs(self.position.y - paddle.get_center().y) < 15:
+            self.direction.x *= -1
+        else:
+            self.direction = (self.position - paddle.get_center()).normalize()
 
     def reset(self):
-        self.x = self.__initial_x
-        self.y = self.__initial_y
-        self.x_dir *= -1
+        self.position.x = self.__initial_x
+        self.position.y = self.__initial_y
+        self.direction.x *= -1
