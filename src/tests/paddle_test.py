7f4c2f775660a07fbe1f6bbea8386b03114a192f
astro <astronomy.dev@gmail.com>
2024-11-22 15:01:13 +0200

    fixed paddle tests

--- src/tests/paddle_test.py
+++ src/tests/paddle_test.py
@@ -7,8 +7,7 @@ class TestPaddle(unittest.TestCase):
     def setUp(self):
         self.start_x = window_size[0]-100
         self.start_y = window_size[1]//2
-        self.paddle = Paddle(window_size, self.start_x,
-                             self.start_y, speed=SPEED, color=(0, 255, 0))
+        self.paddle = Paddle((self.start_x ,self.start_y,))
 
     def test_paddle_in_correct_x_position_at_start(self):
         self.assertEqual(self.start_x, self.paddle.x)
@@ -17,19 +16,19 @@ class TestPaddle(unittest.TestCase):
         self.assertEqual(self.start_y, self.paddle.y)
 
     def test_paddle_moves_correct_amount_on_up_input(self):
-        self.paddle.update(1)
-        self.assertEqual(self.start_y + 5, self.paddle.y)
+        self.paddle.update(window_size, 1)
+        self.assertEqual(self.start_y + self.paddle.speed, self.paddle.y)
 
     def test_paddle_moves_correct_amount_on_down_input(self):
-        self.paddle.update(-1)
-        self.assertEqual(self.start_y - 5, self.paddle.y)
+        self.paddle.update(window_size, -1)
+        self.assertEqual(self.start_y - self.paddle.speed, self.paddle.y)
 
     def test_paddle_does_not_go_off_screen_up(self):
         self.paddle.y = -5
-        self.paddle.update(-1)
+        self.paddle.update(window_size, -1)
         self.assertEqual(0, self.paddle.y)
 
     def test_paddle_does_not_go_off_screen_down(self):
         self.paddle.y = window_size[1] + 1
-        self.paddle.update(1)
+        self.paddle.update(window_size, 1)
         self.assertEqual(window_size[1] - self.paddle.size, self.paddle.y)
