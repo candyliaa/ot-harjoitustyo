a0864896a7922512270fa75f29ac3fe691ebe4c8
astro <astronomy.dev@gmail.com>
2024-11-29 22:32:28 +0200

    config object can now read and write to a json file

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 from sprites.colors import color_dict
+import json
 
 WINDOW_SIZE = (1280, 1024)
 
@@ -26,3 +27,27 @@ class Config:
         self.paddle_color = color_dict["green"]
         self.paddle_speed = PADDLE_SPEED
         self.paddle_size = PADDLE_SIZE
+
+    def write(self):
+        with open("config.json", "w") as f:
+            json.dump(vars(self), f)
+
+    @staticmethod
+    def read():
+        with open("config.json", "r") as f:
+            json_object = json.load(f)
+
+        config = Config()
+        config.difficulty = json_object["difficulty"]
+        config.fps = json_object["fps"]
+        config.window_size = json_object["window_size"]
+
+        config.ball_color = json_object["ball_color"]
+        config.ball_speed = json_object["ball_speed"]
+        config.ball_size = json_object["ball_size"]
+
+        config.paddle_color = json_object["paddle_color"]
+        config.paddle_speed = json_object["paddle_speed"]
+        config.paddle_size = json_object["paddle_size"]
+
+        return config
