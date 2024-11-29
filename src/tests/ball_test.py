282636d3cb0a8669cc26035dfaf94e3d90404a9c
astro <astronomy.dev@gmail.com>
2024-11-29 21:06:07 +0200

    fixed ball and paddle test config errros

--- src/tests/ball_test.py
+++ src/tests/ball_test.py
@@ -1,34 +1,35 @@
 import unittest
 import pygame
 from sprites.ball import Ball
-from config import window_size, SPEED
+from config import Config
 
 
 class TestBall(unittest.TestCase):
     def setUp(self):
-        self.start_x = window_size[0]//2
-        self.start_y = window_size[1]//2
-        self.ball = Ball((self.start_x, self.start_y))
+        self.config = Config()
+        self.start_x = self.config.window_size[0]//2
+        self.start_y = self.config.window_size[1]//2
+        self.ball = Ball((self.start_x, self.start_y), self.config.ball_speed, self.config.ball_color, self.config.ball_size)
 
     def test_ball_in_correct_position_at_start(self):
-        self.assertEqual(self.ball.position, pygame.math.Vector2(window_size[0]//2, window_size[1]//2))
+        self.assertEqual(self.ball.position, pygame.math.Vector2(self.config.window_size[0]//2, self.config.window_size[1]//2))
 
     def test_ball_resets_when_leaves_right(self):
-        self.ball.position.x = window_size[0] + 2 * self.ball.size
-        self.ball.update(window_size)
-        self.assertEqual(self.ball.position, pygame.math.Vector2(window_size[0]//2 - self.ball.speed, window_size[1]//2 + self.ball.speed))
+        self.ball.position.x = self.config.window_size[0] + 2 * self.ball.size
+        self.ball.update(self.config.window_size)
+        self.assertEqual(self.ball.position, pygame.math.Vector2(self.config.window_size[0]//2 - self.ball.speed, self.config.window_size[1]//2 + self.ball.speed))
 
     def test_ball_resets_when_leaves_left(self):
         self.ball.position.x = -self.ball.size
-        self.ball.update(window_size)
-        self.assertEqual(self.ball.position, pygame.math.Vector2(window_size[0]//2 - self.ball.speed, window_size[1]//2 + self.ball.speed))
+        self.ball.update(self.config.window_size)
+        self.assertEqual(self.ball.position, pygame.math.Vector2(self.config.window_size[0]//2 - self.ball.speed, self.config.window_size[1]//2 + self.ball.speed))
 
     def test_ball_bounces_from_ceiling(self):
         self.ball.position.y = 0
-        self.ball.update(window_size)
+        self.ball.update(self.config.window_size)
         self.assertNotEqual(self.ball.position, pygame.math.Vector2(self.ball.position.x, 0))
 
     def test_ball_bounces_from_floor(self):
-        self.ball.position.y = window_size[1] - self.ball.size
-        self.ball.update(window_size)
-        self.assertNotEqual(self.ball.position, pygame.math.Vector2(self.ball.position.x, window_size[1] - self.ball.size))
+        self.ball.position.y = self.config.window_size[1] - self.ball.size
+        self.ball.update(self.config.window_size)
+        self.assertNotEqual(self.ball.position, pygame.math.Vector2(self.ball.position.x, self.config.window_size[1] - self.ball.size))
--- src/tests/paddle_test.py
+++ src/tests/paddle_test.py
@@ -1,13 +1,14 @@
 import unittest
 from sprites.paddle import Paddle
-from config import window_size, SPEED
+from config import Config
 
 
 class TestPaddle(unittest.TestCase):
     def setUp(self):
-        self.start_x = window_size[0]-100
-        self.start_y = window_size[1]//2
-        self.paddle = Paddle((self.start_x ,self.start_y,))
+        self.config = Config()
+        self.start_x = self.config.window_size[0]-100
+        self.start_y = self.config.window_size[1]//2
+        self.paddle = Paddle((self.start_x, self.start_y,), self.config.paddle_speed, self.config.paddle_color, self.config.paddle_size)
 
     def test_paddle_in_correct_x_position_at_start(self):
         self.assertEqual(self.start_x, self.paddle.x)
@@ -16,19 +17,19 @@ class TestPaddle(unittest.TestCase):
         self.assertEqual(self.start_y, self.paddle.y)
 
     def test_paddle_moves_correct_amount_on_up_input(self):
-        self.paddle.update(window_size, 1)
+        self.paddle.update(self.config.window_size, 1)
         self.assertEqual(self.start_y + self.paddle.speed, self.paddle.y)
 
     def test_paddle_moves_correct_amount_on_down_input(self):
-        self.paddle.update(window_size, -1)
+        self.paddle.update(self.config.window_size, -1)
         self.assertEqual(self.start_y - self.paddle.speed, self.paddle.y)
 
     def test_paddle_does_not_go_off_screen_up(self):
         self.paddle.y = -5
-        self.paddle.update(window_size, -1)
+        self.paddle.update(self.config.window_size, -1)
         self.assertEqual(0, self.paddle.y)
 
     def test_paddle_does_not_go_off_screen_down(self):
-        self.paddle.y = window_size[1] + 1
-        self.paddle.update(window_size, 1)
-        self.assertEqual(window_size[1] - self.paddle.size, self.paddle.y)
+        self.paddle.y = self.config.window_size[1] + 1
+        self.paddle.update(self.config.window_size, 1)
+        self.assertEqual(self.config.window_size[1] - self.paddle.size, self.paddle.y)
