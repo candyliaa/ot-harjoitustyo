08c1224bbe6f42c25296813270243c6c8ccfbf7a
astro <astronomy.dev@gmail.com>
2024-12-05 15:54:38 +0200

    added file to read from and write to db

---
+++ src/repositories/stats_repository.py
@@ -0,0 +1,17 @@
+class StatsRepository:
+    def __init__(self, connection):
+        self._connection = connection
+    
+    def get_scores(self):
+        cursor = self._connection.cursor()
+
+        cursor.execute("select * from scores")
+
+        rows = cursor.fetchall()
+
+        return [(row["id"], row["scored"], row["scored_on"]) for row in rows]
+
+    def write_score(self, score, scored_on):
+        cursor = self._connection.cursor()
+
+        cursor.execute(f"insert into scores (scored, scored_on) values ({score}, {scored_on})")
