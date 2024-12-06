daf55523a7498dc427f93de878a8b780fa83d055
astro <astronomy.dev@gmail.com>
2024-12-06 14:43:57 +0200

    fixed cli ui tests

--- src/tests/pongcli_test.py
+++ src/tests/pongcli_test.py
@@ -19,31 +19,31 @@ class TestPongCLI(unittest.TestCase):
         self.config = Config()
         
     def test_ball_speed_gets_changed(self):
-        self.io = MockIO(["3", "1", "20", "6", "4"])
+        self.io = MockIO(["3", "1", "20", "7", "4"])
         self.cli = PongCLI(self.config, self.io, None)
         self.cli.start()
         self.assertEqual(self.config.ball_speed, 20)
 
     def test_ball_color_gets_changed(self):
-        self.io = MockIO(["3", "2", "2", "6", "4"])
+        self.io = MockIO(["3", "2", "2", "7", "4"])
         self.cli = PongCLI(self.config, self.io, None)
         self.cli.start()
         self.assertEqual(self.config.ball_color, (0, 255, 0))
 
     def test_paddle_speed_gets_changed(self):
-        self.io = MockIO(["3", "3", "15", "6", "4"])
+        self.io = MockIO(["3", "3", "15", "7", "4"])
         self.cli = PongCLI(self.config, self.io, None)
         self.cli.start()
         self.assertEqual(self.config.paddle_speed, 15)
 
     def test_paddle_color_gets_changed(self):
-        self.io = MockIO(["3", "4", "1", "6", "4"])
+        self.io = MockIO(["3", "4", "1", "7", "4"])
         self.cli = PongCLI(self.config, self.io, None)
         self.cli.start()
         self.assertEqual(self.config.paddle_color, (255, 0, 0))
 
     def test_ai_difficulty_gets_changed(self):
-        self.io = MockIO(["3", "5", "3", "6", "4"])
+        self.io = MockIO(["3", "5", "3", "7", "4"])
         self.cli = PongCLI(self.config, self.io, None)
         self.cli.start()
         self.assertEqual(self.config.difficulty, 0.8)
