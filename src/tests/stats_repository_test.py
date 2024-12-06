63176571e5d4b31d65658dc2546cd7dec66d464c
astro <astronomy.dev@gmail.com>
2024-12-06 14:44:15 +0200

    functional tests for statsrepository class

--- src/tests/stats_repository_test.py
+++ src/tests/stats_repository_test.py
@@ -5,6 +5,7 @@ from repositories.stats_repository import StatsRepository
 class TestStatsRepository(unittest.TestCase):
     def setUp(self):
         self.connection = sqlite3.connect(":memory:")
+        self.stats = StatsRepository(self.connection)
         self.cursor = self.connection.cursor()
 
         self.cursor.execute("""
@@ -25,3 +26,15 @@ class TestStatsRepository(unittest.TestCase):
                     );
                     """)
         self.connection.commit()
+
+
+
+    def test_scores_written_correctly(self):
+        self.stats.write_score(1, 2)
+        scores = self.stats.get_scores()
+        self.assertEqual(scores, [(1, 1, 2)])
+
+    def test_misc_written_correctly(self):
+        self.stats.write_misc_stats(10, 1000, 1500)
+        misc = self.stats.get_misc_stats()
+        self.assertEqual(misc, [(10, 1000, 1500)])
